cb1da21792fdc44bf9bab67a2562fb6e6fff632e
James Laver <james.laver@gmail.com>
2020-05-17 22:43:39 +0200

    fixes

--- lib/all.ex
+++ lib/all.ex
@@ -11,11 +11,22 @@ defmodule Banshee.All do
   def select(_, navs, data), do: select(navs, data)
 
   def transform(navs, data, transform)
-  def transform(navs, data, transform) do
-    proto(Enumerable, Enum, :map, [data, &Banshee.transform(navs, &1, transform)], data)
+  def transform(navs, data, transform) when is_list(data) do
+    Enum.map(data, &Banshee.transform(navs, &1, transform))
   end
+  def transform(navs, %MapSet{}=data, transform) do
+    Enum.reduce(data, MapSet.new(), fn data, acc ->
+      MapSet.put(acc, Banshee.transform(navs, data, transform))
+    end)
+  end
+  def transform(navs, %{}=data, transform) do
+    Enum.reduce(data, %{}, fn elem, acc ->
+      {k, v} = Banshee.transform(navs, elem, transform)
+      Map.put(acc, k, v)
+    end)
+  end
+  def transform(_, data, _), do: data
 
-  def transform(_param, navs, data, transform)
   def transform(_, navs, data, xf), do: transform(navs, data, xf)
 
 end
